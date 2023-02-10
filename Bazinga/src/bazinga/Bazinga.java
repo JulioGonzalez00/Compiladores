5cc3ab30fac293bf51fbca70ebc55379e1457627
JulioGonzalez00 <83717453+JulioGonzalez00@users.noreply.github.com>
2023-02-09 18:09:56 -0600

    Primer commit

---
+++ Bazinga/src/bazinga/Bazinga.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bazinga;
+
+import GUI.Principal;
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author julio
+ */
+public class Bazinga {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new FlatMacDarkLaf());
+        } catch (UnsupportedLookAndFeelException ex) {
+            JOptionPane.showMessageDialog(null,"Error Kitsune:\n"+ex.getMessage());
+            System.exit(0);
+        }
+        Principal p = new Principal();
+        p.setLocationRelativeTo(null);
+        p.setVisible(true);
+    }
+
+}
